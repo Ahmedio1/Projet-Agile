697da5df87f960b6ba2644fb53a6bb592bc19ae1
ahmedo <ahmedo@kali.Machine>
2022-04-04 23:00:08 +0200

    :white_check_mark: add Models equipe equipe joueur and joueur

---
+++ src/app/models/equipe_joueur.ts
@@ -0,0 +1,5 @@
+export interface Equipe_joueur {
+  id_equipe_joueur: number;
+  id_equipe: number;
+  id_joueur:number;
+}
---
+++ src/app/models/joueur.ts
@@ -0,0 +1,9 @@
+export interface Joueur {
+  id_joueur: number;
+  mail: string;
+  age: number;
+  score: number;
+  temps: number;
+  avatar_url: string;
+  pseaudo: string;
+}
